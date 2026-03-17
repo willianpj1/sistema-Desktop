5a4d4a4d79da58690905230e58da1c3807024c5c
willianpj1 <wjeronimo22@gmail.com>
2026-03-16 21:41:52 -0400

    commit
    commit

--- app/renderer/main/produto.js
+++ app/renderer/main/produto.js
@@ -8,8 +8,18 @@ saveButton.addEventListener('click', async () => {
 
     try {
         const result = await window.electronAPI.saveProduct(data);
+        Swal.fire({
+            title: "Produto salvo com sucesso!",
+            icon: "success",
+            draggable: true
+        });
         console.log('Produto salvo com sucesso:', result);
     } catch (error) {
+        Swal.fire({
+            title: "Erro ao salvar produto:",
+            text:  error,
+            icon: "error"
+        });
         console.error('Erro ao salvar produto:', error);
     }
 });
